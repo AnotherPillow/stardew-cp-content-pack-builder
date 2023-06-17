1b56b0da14cc30e53822c78aea0b791a7294c9fb
AnotherPillow <Thepilous_@outlook.com>
2023-06-17 21:45:16 +0100

    Mirror  "Fix automatic LogName"

--- renderer/index.js
+++ renderer/index.js
@@ -467,7 +467,7 @@ function addChange(ev) {
     const logNameInput = document.createElement("input");
     logNameInput.classList.add("lognameinput");
     logNameInput.type = "text";
-    logNameInput.placeholder = "Log Name";
+    logNameInput.placeholder = "Log Name (optional)";
 
     // * target/asset dropdowns
     const dropdownElements = document.createElement("div");
@@ -732,6 +732,7 @@ function handleContentUpdate(ev) {
 
     value["Changes"] = [];
     value["CustomLocations"] = [];
+    value["ConfigSchema"] = {};
 
     const allCustomLocations = document.querySelectorAll(".individuallocation");
     allCustomLocations.forEach(location => {
@@ -914,8 +915,8 @@ function handleContentUpdate(ev) {
         if (logNameElement.value) {
             changeObject.LogName = logNameElement.value;
         }
-        else if (changeObject.Target !== undefined && !logNameElement.value) {
-            changeObject.LogName = changeObject.Target;
+        else if ((targetElement.value !== "(Target)") && (logNameElement.value.length === 0)) {
+            changeObject.LogName = targetElement.value;
         }
 
         value["Changes"].push(changeObject);
